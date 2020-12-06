5b86be2993e053da30537ae78158b88e6dfafb01
jassam <jadliaissam@gmail.com>
2020-12-06 18:38:41 +0100

    fix static heroku

--- sharedlingo/urls.py
+++ sharedlingo/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('front/', include('frontend.urls')),
+    path('', include('frontend.urls')),
     url(r'^api-auth/', include('rest_framework.urls'))
 
 ]
